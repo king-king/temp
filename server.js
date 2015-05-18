d7772ef9f63963a52252b246f706477ed525e62a
WQ <niuyiwangqun@sohu.com>
2015-05-18 16:21:09 +0800

    c

--- server.js
+++ server.js
@@ -4,16 +4,13 @@
 
 var http = require( "http" );
 
-var a = 0;
 
 http.createServer( function ( req, res ) {
-    var time = parseInt( req.url.slice( 1 ) );
-    a += 10;
-    setTimeout( function () {
-        res.writeHead( 200, {
-            'Content-Type' : 'text/plain',
-            "Access-Control-Allow-Origin" : "*"
-        } );
-        res.end( time + "  a=" + a );
-    }, time * 1000 );
-} ).listen( 8181, 'localhost' );
+    console.log( "ok" );
+    res.writeHead( 200, {
+        'Content-Type' : 'text/plain',
+        "Access-Control-Allow-Origin" : "*"
+    } );
+    res.write( "yes" );
+    res.end();
+} ).listen( 8585 );
