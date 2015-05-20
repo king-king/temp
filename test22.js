3bbb743bae7a6dccfd67b0c3579bb1ec5a88c437
WQ <niuyiwangqun@sohu.com>
2015-05-20 15:23:17 +0800

    c

--- test22.js
+++ test22.js
@@ -1,3 +1,12 @@
-var s = require( "zach-sprite" );
+var zip = require( "zach-zip" );
 
-console.log( s );
+var s = (new Date()).getTime();
+
+zip.add( {
+    output : "deploy.zip",
+    input : "firstpage3"
+}, function ( err, str ) {
+    console.log( (new Date()).getTime() - s )
+} );
+
+
