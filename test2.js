bc5f03b32d26e9d5242d55c5e0c22a12b689c489
WQ <niuyiwangqun@sohu.com>
2015-06-09 17:13:35 +0800

    c

---
+++ test2.js
@@ -0,0 +1,4 @@
+var a = {
+    "code" : 500,
+    "message" : "The parameters dictionary contains a null entry for parameter \u0027omid\u0027 of non-nullable type \u0027System.Int32\u0027 for method \u0027System.Web.Mvc.JsonResult WebEditorUploador(Int32, System.Guid, Cloud7BeginPage.Web.ViewModels.CropImageViewModel, System.String, Boolean)\u0027 in \u0027Cloud7BeginPage.Web.Controllers.MediaController\u0027. An optional parameter must be a reference type, a nullable type, or be declared as an optional parameter.\r\nParameter name: parameters"
+}
