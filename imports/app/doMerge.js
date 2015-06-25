d7369ae8f9b82707d387f129b14c5a9e3eba992e
WQ <niuyiwangqun@sohu.com>
2015-06-25 13:40:06 +0800

    c

--- imports/app/doMerge.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by WQ on 2015/6/25.
- */
-
-var merge = require( "./../merge.js" );
-
-merge( "lib/main.js", "lib/main.min.js" );
---
+++ imports/doMerge.js
@@ -0,0 +1,7 @@
+/**
+ * Created by WQ on 2015/6/25.
+ */
+
+var merge = require( "./merge.js" );
+
+merge( "app/lib/main.js", "app/lib/main.min.js" );
