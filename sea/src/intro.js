e2208193ff4fdc2a4b4f2b94b72199d1e6a578b9
WQ <niuyiwangqun@sohu.com>
2015-06-25 15:14:06 +0800

    c

--- sea/src/intro.js
+++ sea/src/intro.js
@@ -1,9 +1,10 @@
 /**
  * Sea.js @VERSION | seajs.org/LICENSE.md
  */
-(function(global, undefined) {
+(function ( global, undefined ) {
 
 // Avoid conflicting when `sea.js` is loaded multiple times
-if (global.seajs) {
-  return
-}
+    if ( global.seajs ) {
+        return
+    }
+})();
