43dbdf59ef2d1be6255127b3707472b16533ce39
WQ <niuyiwangqun@sohu.com>
2015-08-24 14:34:45 +0800

    a

---
+++ special-effect-preview/filter-desaturate.js
@@ -0,0 +1,21 @@
+/**
+ * Created by WQ on 2015/6/2.
+ *
+ * 颜色去饱和，使之变为灰色图像（或者说是黑白图）
+ *
+ */
+
+(function () {
+    function desaturate( imgData ) {
+        var imgDataResult = document.createElement( "canvas" ).getContext( "2d" ).createImageData( imgData ),
+            av;
+        for ( var i = 0; i < imgData.data.length; i += 4 ) {
+            av = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3;
+            imgDataResult.data[i] = imgDataResult.data[i + 1] = imgDataResult.data[i + 2] = av;
+            imgDataResult.data[i + 3] = 255;
+        }
+        return imgDataResult;
+    }
+
+    window.desaturate = desaturate;
+})();
