aa72a55001cbf47725dd3324ad0c4d92e878bc13
WQ <niuyiwangqun@sohu.com>
2015-09-02 14:28:52 +0800

    containsNearbyDuplicate

--- test.js
+++ test.js
@@ -1,38 +1,39 @@
-var majorityElement = function ( nums ) {
-    var result = [];
-    if ( nums.length == 0 ) {
-        return [];
+var containsNearbyDuplicate = function ( nums, k ) {
+    if ( !k ) {
+        return false;
     }
-    if ( nums.length < 3 ) {
-        nums[0] && result.push( nums[0] );
-        nums[1] && nums[1] != nums[0] && result.push( nums[1] );
-        return result;
-    }
-    var arr = nums.sort( function ( a, b ) {
-        return a - b;
-    } );
-
-    var len_3 = arr.length / 3,
-        len_23 = (( arr.length * 2 / 3) << 0) + 1,
-        count = 0,
-        curval = arr[0];
-
-    for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i] == curval ) {
-            count += 1;
-            if ( i == len_23 - 1 ) {
-                if ( count > len_3 ) {
-                    result.push( arr[i] );
-                }
+    var len = nums.length;
+    var arg = {};
+    if ( len <= k ) {
+        for ( var i = 0; i < len; i++ ) {
+            if ( arg[nums[i]] ) {
+                return true;
+            }
+            else {
+                arg[nums[i]] = true;
             }
         }
-        else {
-            if ( count > len_3 ) {
-                result.push( arr[i - 1] );
-            }
-            curval = arr[i];
-            count = 0;
-        }
+        return false;
     }
-    return result;
+    else {
+        var j;
+        for ( j = 0; j <= k; j++ ) {
+            if ( arg[nums[j]] ) {
+                return true;
+            }
+            else {
+                arg[nums[j]] = true;
+            }
+        }
+        for ( ; j < len; j++ ) {
+            arg[nums[j - k - 1]] = false;
+            if ( arg[nums[j]] ) {
+                return true;
+            }
+            else {
+                arg[nums[j]] = true;
+            }
+        }
+        return false;
+    }
 };
