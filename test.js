454ab312984782e1f2e32760b8ca11b35deb750c
WQ <niuyiwangqun@sohu.com>
2015-09-02 09:47:34 +0800

    addDigits

--- test.js
+++ test.js
@@ -1,57 +1,25 @@
-cc.Class = function () {
-};
-cc.Class.extend = function ( prop ) {
-    var _super = this.prototype;
+var addDigits = function ( num ) {
 
-    // Instantiate a base class (but only create the instance, 
-    // don't run the init constructor) 
-    window.initializing = true;
-    var prototype = new this();
-    window.initializing = false;
-    window.fnTest = /xyz/.test( function () {
-        xyz;
-    } ) ? /\b_super\b/ : /.*/;
-
-    // Copy the properties over onto the new prototype 
-    for ( var name in prop ) {
-        // Check if we're overwriting an existing function 
-        prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && fnTest.test( prop[name] ) ?
-            (function ( name, fn ) {
-                return function () {
-                    var tmp = this._super;
-
-                    // Add a new ._super() method that is the same method 
-                    // but on the super-class 
-                    this._super = _super[name];
-
-                    // The method only need to be bound temporarily, so we 
-                    // remove it when we're done executing 
-                    var ret = fn.apply( this, arguments );
-                    this._super = tmp;
-
-                    return ret;
-                };
-            })( name, prop[name] ) :
-            prop[name];
-    }
-
-    // The dummy class constructor 
-    function Class() {
-        // All construction is actually done in the init method 
-        if ( !window.initializing && this.ctor ) {
-            this.ctor.apply( this, arguments );
+    function decompose( n ) {
+        var result = [];
+        while ( n >= 10 ) {
+            result.push( n % 10 );
+            n = n / 10 << 0;
         }
+        result.push( n );
+        return result;
     }
 
-    // Populate our constructed prototype object 
-    Class.prototype = prototype;
+    function add( arr ) {
+        var result = 0;
+        for ( var i = 0; i < arr.length; i++ ) {
+            result += arr[i];
+        }
+        return result;
+    }
 
-    // Enforce the constructor to be what we expect 
-    Class.prototype.constructor = Class;
-
-    // And make this class extendable 
-    Class.extend = arguments.callee;
-
-    return Class;
-}; 
+    while ( num >= 10 ) {
+        num = add( decompose( num ) );
+    }
+    return num;
+};
