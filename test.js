7c1122acf9a3edad8c1fcd9b69695563d62149d2
WQ <niuyiwangqun@sohu.com>
2015-09-02 11:32:44 +0800

    majorityElement 方案1

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
-var deleteNode = function ( node ) {
-    if ( !node.next ) {
-        node = null;
+var majorityElement = function ( nums ) {
+    var arg = {};
+    for ( var i = 0; i < nums.length; i++ ) {
+        if ( !arg[nums[i]] ) {
+            arg[nums[i]] = 1;
+        }
+        else {
+            arg[nums[i]] += 1;
+        }
     }
-    else {
-        node.val = node.next.val;
-        node.next = node.next.next;
+    var res = [], len = nums.length / 3 << 0;
+    for ( var key in arg ) {
+        if ( arg[key] > len ) {
+            res.push( key );
+        }
     }
+    return res;
 };
