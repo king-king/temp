a0c7aa62ad5f5effd8e10bf9ee0dd54ddafe2f8d
WQ <niuyiwangqun@sohu.com>
2015-09-02 17:36:26 +0800

    lengthOfLastWord 方案二 稍慢

--- test.js
+++ test.js
@@ -1,49 +1,10 @@
-var containsNearbyAlmostDuplicate = function ( nums, k, t ) {
-    if ( k == 0 && t != 0 ) {
-        return false;
-    }
-    var len = nums.length;
-    var min = nums[0];
-    var i, j;
-    var abs = Math.abs;
-    if ( len <= k ) {
-        for ( i = 0; i <= len; i++ ) {
-            for ( j = i + 1; j <= len; j++ ) {
-                if ( min > abs( nums[i] - nums[j] ) ) {
-                    min = abs( nums[i] - nums[j] );
-                    if ( min <= t ) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+var lengthOfLastWord = function ( s ) {
+    s = s.trim();
+    if ( !s ) {
+        return 0;
     }
     else {
-        min = abs( nums[0] - nums[1] );
-        if ( min <= t ) {
-            return true;
-        }
-        for ( i = 0; i <= k; i++ ) {
-            for ( j = i + 1; j <= k; j++ ) {
-                if ( min > abs( nums[i] - nums[j] ) ) {
-                    min = abs( nums[i] - nums[j] );
-                    if ( min <= t ) {
-                        return true;
-                    }
-                }
-            }
-        }
-        for ( i = k + 1; i < len; i++ ) {
-            for ( j = i - k; j < i; j++ ) {
-                if ( min > abs( nums[i] - nums[j] ) ) {
-                    min = abs( nums[i] - nums[j] );
-                    if ( min <= t ) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        var arr = s.split( " " );
+        return arr[arr.length - 1].length;
     }
 };
