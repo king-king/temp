02c83ff2372a24597543e38b1b2d0d84707c6df6
WQ <niuyiwangqun@sohu.com>
2015-09-02 18:03:54 +0800

    majorityElement

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-var lengthOfLastWord = function ( s ) {
-    s = s.trim();
-    if ( !s ) {
-        return 0;
+var majorityElement = function ( nums ) {
+    if ( nums.length == 0 ) {
+        return nums[0];
     }
-    else {
-        var arr = s.split( " " );
-        return arr[arr.length - 1].length;
+    var arg = {};
+    var threshold = nums.length / 2 << 0;
+    for ( var i = 0; i < nums.length; i++ ) {
+        if ( !arg[nums[i]] ) {
+            arg[nums[i]] = 1;
+        }
+        else {
+            arg[nums[i]] += 1;
+            if ( arg[nums[i]] > threshold ) {
+                return nums[i];
+            }
+        }
     }
 };
