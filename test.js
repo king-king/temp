4a2d9d285ea33dcf6f3858743f5aebb163d9dbb4
king-king <niuyiwangqun@sohu.com>
2015-09-03 20:28:13 +0800

    reverseList 方案一 速度较慢

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
-var majorityElement = function ( nums ) {
-    if ( nums.length == 0 ) {
-        return nums[0];
+var reverseList = function (head) {
+    if (!head) {
+        return head;
     }
-    var arg = {};
-    var threshold = nums.length / 2 << 0;
-    for ( var i = 0; i < nums.length; i++ ) {
-        if ( !arg[nums[i]] ) {
-            arg[nums[i]] = 1;
-        }
-        else {
-            arg[nums[i]] += 1;
-            if ( arg[nums[i]] > threshold ) {
-                return nums[i];
-            }
-        }
+
+    function ListNode(val) {
+        this.val = val;
+        this.next = null;
     }
+
+    var arr = [];
+    var p = head;
+    while (p) {
+        arr.push(new ListNode(p.val));
+        p = p.next;
+    }
+    var len = arr.length - 1;
+    for (var i = len; i >= 0; i--) {
+        arr[i].next = arr[i - 1];
+    }
+    return arr[len];
 };
