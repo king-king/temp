41ae31270c3d55c014016cb3e88562b6c04ce8b4
WQ <niuyiwangqun@sohu.com>
2015-09-06 16:32:12 +0800

    maxSlidingWindow 非常快

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
-var removeElements = function ( head, val ) {
-    while ( head && head.val == val ) {
-        head = head.next;
-    }
-    if ( !head ) {
-        return head;
-    }
-    var pre = head;
-    var p = head.next;
-    while ( p ) {
-        if ( p.val == val ) {
-            pre.next = p.next;
-        }
-        else {
-            pre = p;
-        }
-        p = p.next;
-    }
-    return head;
-};
+/*
+ Given an array nums, there is a sliding window of size k which is moving
+ from the very left of the array to the very right. You can only see the k
+ numbers in the window. Each time the sliding window moves right by one position.
+
+ For example,
+ Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+ Window position                Max
+ ---------------               -----
+ [1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+ Therefore, return the max sliding window as [3,3,5,5,6,7].
+
+ */
