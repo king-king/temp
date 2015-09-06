583255be9c953e8ea0eecef41425e096891c9c0a
WQ <niuyiwangqun@sohu.com>
2015-09-06 15:59:49 +0800

    removeElements

--- test.js
+++ test.js
@@ -1,53 +1,20 @@
-/**
- * @constructor
- */
-var MinStack = function () {
-    this.stack = [];
-    this.min = 0;
-    this.length = 0;
-};
-
-/**
- * @param {number} x
- * @returns {void}
- */
-MinStack.prototype.push = function (x) {
-    this.length += 1;
-    this.stack.push(x);
-    if (this.length == 1) {
-        this.min = x;
+var removeElements = function ( head, val ) {
+    while ( head && head.val == val ) {
+        head = head.next;
     }
-    if (x < this.min) {
-        this.min = x;
+    if ( !head ) {
+        return head;
     }
-};
-
-/**
- * @returns {void}
- */
-MinStack.prototype.pop = function () {
-    this.length -= 1;
-    var val = this.stack.pop();
-    if (val == this.min) {
-        var arr = JSON.parse(JSON.stringify(this.stack));
-        arr.sort(function (a, b) {
-            return a - b;
-        });
-        this.min = arr[0];
+    var pre = head;
+    var p = head.next;
+    while ( p ) {
+        if ( p.val == val ) {
+            pre.next = p.next;
+        }
+        else {
+            pre = p;
+        }
+        p = p.next;
     }
-};
-
-/**
- * @returns {number}
- */
-MinStack.prototype.top = function () {
-    return this.stack[this.stack.length - 1];
-};
-
-/**
- * @returns {number}
- */
-MinStack.prototype.getMin = function () {
-    console.log(this.min);
-    return this.min;
+    return head;
 };
