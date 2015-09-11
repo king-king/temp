bfa245b2ba4ba2970555a76e11652b7c2b507cc5
WQ <niuyiwangqun@sohu.com>
2015-09-11 10:02:50 +0800

    c

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ a.style.height = "100px";
 a.style.width = "200px";
 a.style.position = "absolute";
 a.style.background = "red";
-a.style.webkitTransition = "2s linear 0s";
 $0.body.appendChild( a );
 
 var s;
@@ -13,6 +12,7 @@ a.addEventListener( "webkitTransitionEnd", function () {
     console.log( (new Date()).getTime() - s )
 }, false );
 a.onclick = function () {
+    a.style.webkitTransition = "2s linear 0s";
     a.style.left = "200px";
     s = (new Date()).getTime();
 };
