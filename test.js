d5ad16791fd799b89a464e6d0b53a9581091b60d
WQ <niuyiwangqun@sohu.com>
2015-09-24 13:25:11 +0800

    c

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-var a = {"0" : "blur(14px)", "23" : "blur(14px)", "36" : "blur(18px)", "54" : "blur(18px)", "100" : "blur(1px)"}
+var a = {
+    "0" : "blur(14px)",
+    "23" : "blur(14px)",
+    "36" : "blur(18px)",
+    "54" : "blur(18px)",
+    "100" : "blur(1px)"
+};
