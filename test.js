37bb27b43f2df8b8643baea1ef2d7ac21c9cb4c0
WQ <niuyiwangqun@sohu.com>
2015-09-25 11:02:35 +0800

    c

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-var a = {
-    "0" : "blur(14px)",
-    "23" : "blur(14px)",
-    "36" : "blur(18px)",
-    "54" : "blur(18px)",
-    "100" : "blur(1px)"
-};
+var a = {"100" : "translate3d(0%,0%,0) scale(1) rotate(0deg)}"}
