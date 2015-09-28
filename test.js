fe3410b2fb078534e131a08ab5ca7e4dc27f3cac
WQ <niuyiwangqun@sohu.com>
2015-09-28 17:28:48 +0800

    添加新功能，改小样式

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-a = {"0": "translate3d(0%,0%,0) scale(1.8) rotate(0deg)}", "100": "translate3d(0%,0%,0) scale(1) rotate(0deg)}"}
+a = {
+    "0" : "blur(3px)",
+    "30" : "blur(3px)",
+    "50" : "blur(10px)",
+    "100" : "blur(0px)"
+};
+
+
+b = {
+    "0" : "grayscale(0.4) sepia(0.2) saturate(1.2) hue-rotate(45deg) invert(0.1) brightness(1.2) contrast(1.3) blur(2px)",
+    "43" : "blur(9px)",
+    "100" : "blur(0px)"
+}
