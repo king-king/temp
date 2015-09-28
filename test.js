8a120a9146fc5f111179629250a3e7a3ef663f63
WQ <niuyiwangqun@sohu.com>
2015-09-28 17:53:37 +0800

    完善滤镜导入

--- test.js
+++ test.js
@@ -6,8 +6,4 @@ a = {
 };
 
 
-b = {
-    "0" : "grayscale(0.4) sepia(0.2) saturate(1.2) hue-rotate(45deg) invert(0.1) brightness(1.2) contrast(1.3) blur(2px)",
-    "43" : "blur(9px)",
-    "100" : "blur(0px)"
-}
+b = {"0" : "blur(15px)", "100" : "blur(0px)"}
