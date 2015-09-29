eaab870df9f556f986a2dd9f6795b9de3b1f3e78
WQ <niuyiwangqun@sohu.com>
2015-09-29 17:03:40 +0800

    改bug

--- test.js
+++ test.js
@@ -11,3 +11,23 @@ b = {
     "64" : "translate3d(-29%,0%,0) scale(1.6) rotate(0deg)",
     "100" : "translate3d(0%,0%,0) scale(1) rotate(0deg)"
 };
+
+c = {
+    "0" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)",
+
+    "2" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)",
+
+    "18" : "hue-rotate(45deg) brightness(1.1) blur(1px) saturate(1)",
+
+    "24" : "hue-rotate(45deg) brightness(1.1) blur(1px) saturate(1)",
+
+    "40" : "hue-rotate(123deg) brightness(0.9) blur(0px) saturate(1)",
+
+    "44" : "hue-rotate(123deg) brightness(0.9) blur(0px) saturate(1)",
+
+    "66" : "hue-rotate(181deg) brightness(1.1) blur(1px) saturate(1)",
+
+    "69" : "hue-rotate(181deg) brightness(1.1) blur(1px) saturate(1)",
+
+    "100" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)"
+}
