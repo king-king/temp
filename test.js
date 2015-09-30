219aa419c580bd252c6adb47e6442a480e4e8f8e
WQ <niuyiwangqun@sohu.com>
2015-09-30 10:24:09 +0800

    删减冗余代码

--- test.js
+++ test.js
@@ -13,21 +13,6 @@ b = {
 };
 
 c = {
-    "0" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)",
-
-    "2" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)",
-
-    "18" : "hue-rotate(45deg) brightness(1.1) blur(1px) saturate(1)",
-
-    "24" : "hue-rotate(45deg) brightness(1.1) blur(1px) saturate(1)",
-
-    "40" : "hue-rotate(123deg) brightness(0.9) blur(0px) saturate(1)",
-
-    "44" : "hue-rotate(123deg) brightness(0.9) blur(0px) saturate(1)",
-
-    "66" : "hue-rotate(181deg) brightness(1.1) blur(1px) saturate(1)",
-
-    "69" : "hue-rotate(181deg) brightness(1.1) blur(1px) saturate(1)",
-
-    "100" : "hue-rotate(0deg) brightness(1) blur(0px) saturate(0)"
-}
+    "0" : "translate3d(0%,0%,0) scale(2.5) rotate(0deg)",
+    "52" : "translate3d(0%,0%,0) scale(1) rotate(0deg)"
+};
