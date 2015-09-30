3eaac856497249bb60db59e70e7467c6b1b6b31a
WQ <niuyiwangqun@sohu.com>
2015-09-30 10:26:17 +0800

    删减冗余代码

--- test.js
+++ test.js
@@ -12,7 +12,4 @@ b = {
     "100" : "translate3d(0%,0%,0) scale(1) rotate(0deg)"
 };
 
-c = {
-    "0" : "translate3d(0%,0%,0) scale(2.5) rotate(0deg)",
-    "52" : "translate3d(0%,0%,0) scale(1) rotate(0deg)"
-};
+c = {"0" : "translate3d(0%,0%,0) scale(2.6) rotate(0deg)", "100" : "translate3d(0%,0%,0) scale(1) rotate(0deg)"}
