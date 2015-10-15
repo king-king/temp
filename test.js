c1186ee2615d1f72515df48fc7d77f17f42edcff
WQ <niuyiwangqun@sohu.com>
2015-10-15 13:30:15 +0800

    c

--- test.js
+++ test.js
@@ -10,6 +10,6 @@ b = { "0" : "blur(5px)" , "100" : "blur(0px)" }
 c = {
     "0" : "translate3d(0%,0%,0) scale(2.1) rotate(0deg) opacity(1)" ,
     "100" : "translate3d(0%,0%,0) scale(1) rotate(0deg) opacity(1)"
-};
+}
 
 v = { "0" : "blur(6px)" , "100" : "blur(0px)" }
