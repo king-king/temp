76caff418574210d447f66fb049cf287a07611c5
WQ <niuyiwangqun@sohu.com>
2015-10-20 15:11:44 +0800

    字体压缩试验

--- font-test/g-font.js
+++ font-test/g-font.js
@@ -1,32 +1,12 @@
-/**
- * Created by WQ on 2015/10/20.
- */
 var Fontmin = require( 'fontmin' );
 
-var srcPath = "zw.tff"; // 字体源文件
-var destPath = "/font";    // 输出路径
-var text = '我说你是人间的四月天；笑响点亮了四面风；轻灵在春的光艳中交舞着变。';
+var fontmin = new Fontmin().src( 'zw.ttf' ).dest( 'font' ).use( Fontmin.glyph( {
+    text : '天地玄黄 宇宙洪荒' ,
+    hinting : false         // keep ttf hint info (fpgm, prep, cvt). default = true
+} ) );
 
-// 初始化
-var fontmin = new Fontmin()
-    .src( srcPath )               // 输入配置
-    .use( Fontmin.glyph( {        // 字型提取插件
-        text : text              // 所需文字
-    } ) );
-//.use( Fontmin.ttf2eot() )     // eot 转换插件
-//.use( Fontmin.ttf2woff() )    // woff 转换插件
-//.use( Fontmin.ttf2svg() )     // svg 转换插件
-//.use( Fontmin.css() )         // css 生成插件
-//.dest( destPath );            // 输出配置
-
-// 执行
-fontmin.run( function ( err , files , stream ) {
-
-    if ( err ) {                  // 异常捕捉
-        console.error( err );
-    } else {
-        console.log( 'done' );        // 成功
-        console.log( files )
+fontmin.run( function ( err , files ) {
+    if ( err ) {
+        console.log( err );
     }
-
 } );
