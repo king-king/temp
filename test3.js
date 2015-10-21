a57f8959e2aded05d0b9ce8426fec7fdd4f2fb63
WQ <niuyiwangqun@sohu.com>
2015-10-21 16:55:43 +0800

    c

--- test3.js
+++ test3.js
@@ -1,6 +1,10 @@
 var a = {
-    "0" : "translate3d(0,0,0) scale(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) opacity(1)" ,
-    "100" : " rotateY(10deg) "
+    "0" : "translate3d(0%,0%,0)  scale(1)  rotateX(0deg)  rotateY(0deg)  rotateZ(0deg)  opacity(1) " ,
+    "14" : " rotateY(10deg) " ,
+    "26" : " rotateY(-10deg) " ,
+    "43" : " rotateY(10deg) " ,
+    "73" : " rotateY(-10deg) " ,
+    "100" : " rotateY(0deg) "
 }
 
-
+b = { "0" : "grayscale(0) sepia(0)" , "100" : "grayscale(0.5) sepia(0.7)" }
