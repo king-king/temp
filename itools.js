d656a6b0ca947d13554228fecd09a5e2b3b1f62d
WQ <niuyiwangqun@sohu.com>
2015-10-22 10:35:47 +0800

    c

--- itools.js
+++ itools.js
@@ -4,61 +4,58 @@
 
 var querySelector = document.querySelector.bind( document );
 
-function loopArray( arr, func ) {
+function loopArray( arr , func ) {
     for ( var i = 0; i < arr.length; i++ ) {
-        func( arr[i], i );
+        func( arr[ i ] , i );
     }
 }
 
-function loop( i, func ) {
+function loop( i , func ) {
     for ( var n = 0; n < i; n++ ) {
         func( n );
     }
 }
 
-// ²¢·¢
-function concurrentTask( tasks, callback ) {
-    var len = tasks.length,
-        count = 0;
+// ï¿½ï¿½ï¿½ï¿½
+function concurrentTask( tasks , callback ) {
+    var len = tasks.length , count = 0;
     tasks.forEach( function ( task ) {
         task( function () {
             count++;
-            if ( count == len ) {
-                callback();
-            }
+            count == len && callback();
         } );
     } );
 }
 
-function map( array, func ) {
+function map( array , func ) {
     var mapArray = [];
-    array.forEach( function ( item, i ) {
-        mapArray.push( func( item, i ) );
+    array.forEach( function ( item , i ) {
+        mapArray.push( func( item , i ) );
     } );
     return mapArray;
 }
 
 
-function element( tag, arg, parent ) {
+function element( tag , arg , parent ) {
     var el = document.createElement( tag );
     for ( var key in arg ) {
         switch ( key ) {
             case "classList":
-                loopArray( arg.classList, function ( klass ) {
+                loopArray( arg.classList , function ( klass ) {
                     el.classList.add( klass );
                 } );
                 break;
             default :
-                el[key] = arg[key];
+                el[ key ] = arg[ key ];
         }
     }
     parent && parent.appendChild( el );
     return el;
 }
 
-function css( el, style ) {
+function css( el , style ) {
     for ( var key in style ) {
-        el.style.setProperty( key, style[key], null );
+        el.style.setProperty( key , style[ key ] , null );
     }
     return el;
 }
