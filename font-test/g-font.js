3c0ec13ceac9dda13ebb1130a6a5b35403be47a0
WQ <niuyiwangqun@sohu.com>
2015-10-23 10:32:20 +0800

    c

--- font-test/g-font.js
+++ font-test/g-font.js
@@ -1,8 +1,8 @@
 var Fontmin = require( 'fontmin' );
 
 var fontmin = new Fontmin().src( 'zw.ttf' ).dest( 'font' ).use( Fontmin.glyph( {
-    text : '天地玄黄 宇宙洪荒' ,
-    hinting : false         // keep ttf hint info (fpgm, prep, cvt). default = true
+    text : '白日依山尽，黄河入海流' ,
+    hinting : false
 } ) );
 fontmin.run( function ( err , files ) {
     if ( err ) {
