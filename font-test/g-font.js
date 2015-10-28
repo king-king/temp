20d8ab3097c2ff0614272eb79bb27a9676fa0521
WQ <niuyiwangqun@sohu.com>
2015-10-28 17:59:13 +0800

    c

--- font-test/g-font.js
+++ font-test/g-font.js
@@ -1,7 +1,7 @@
 var Fontmin = require( 'fontmin' );
 
 var fontmin = new Fontmin().src( 'zw.ttf' ).dest( 'font' ).use( Fontmin.glyph( {
-    text : '白日依山尽，黄河入海流' ,
+    text : '白日依山尽，黄河入海流' , // 要保留的文字
     hinting : false
 } ) );
 fontmin.run( function ( err , files ) {
