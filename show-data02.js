c09909b3b4c881035bce5f7a7126706c8edc4596
WQ <niuyiwangqun@sohu.com>
2015-12-25 13:52:13 +0800

    c

---
+++ show-data02.js
@@ -0,0 +1,26 @@
+/**
+ * Created by WQ on 2015/12/25.
+ */
+
+var data = [
+    {
+        img : "img/show/2/show-mv-manhua.png" ,
+        name : "mv-漫画" ,
+        src : "http://chuyebeta.cloud7.com.cn/15498854"
+    } ,
+    {
+        img : "img/show/2/show-mv-laopao.png" ,
+        name : "mv-老炮" ,
+        src : "http://chuyebeta.cloud7.com.cn/15581870"
+    } ,
+    {
+        img : "img/show/2/show-mv-sd.png" ,
+        name : "mv-圣诞" ,
+        src : "http://chuyebeta.cloud7.com.cn/15602818"
+    } ,
+    {
+        img : "img/show/2/show-mv-banshi01.png" ,
+        name : "版式-集合" ,
+        src : "http://chuye.cloud7.com.cn/15714380"
+    }
+];
