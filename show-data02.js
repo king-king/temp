8174f5ad60d3f90b30fc75ca02cf3ef8bc350aa6
WQ <niuyiwangqun@sohu.com>
2015-12-25 14:18:52 +0800

    c

--- show-data02.js
+++ show-data02.js
@@ -19,8 +19,22 @@ var data = [
         src : "http://chuyebeta.cloud7.com.cn/15602818"
     } ,
     {
+        img : "img/show/2/show-haibao.png" ,
+        name : "海报-跨年告白" ,
+        src : "http://192.168.0.229:55554/test100.html"
+    } ,
+    {
+        img : "img/show/2/show-hand.png" ,
+        name : "手工-把爱说开" ,
+        src : "http://chuye.cloud7.com.cn/vips/saylove/index.html"
+    } ,
+    {
         img : "img/show/2/show-mv-banshi01.png" ,
-        name : "版式-集合" ,
+        name : "版式-集合01" ,
         src : "http://chuye.cloud7.com.cn/15714380"
+    } , {
+        img : "img/show/2/show-mv-banshi02.png" ,
+        name : "版式-集合02" ,
+        scr : "http://chuyebeta.cloud7.com.cn/15584651"
     }
 ];
