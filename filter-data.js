bcfa6bd9f2f135d6c2ada6cb1edeb2ebaeb23b0b
WQ <niuyiwangqun@sohu.com>
2016-01-07 16:32:16 +0800

    c

---
+++ filter-data.js
@@ -0,0 +1,12 @@
+/**
+ * Created by WQ on 2016/1/7.
+ */
+
+// ±äÄ£ºı
+var a = {
+    p : {
+        20 : "contrast(1) saturate(1) blur(0px)" ,
+        100 : " contrast(1.4) saturate(1.2) blur(4px)"
+    } ,
+    d : 3
+};
