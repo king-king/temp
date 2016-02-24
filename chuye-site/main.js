82216e97cd1d44471c92f1fabfd44ba758897120
WQ <niuyiwangqun@sohu.com>
2016-02-24 12:34:34 +0800

    c

--- chuye-site/main.js
+++ chuye-site/main.js
@@ -1,3 +1,29 @@
 /**
  * Created by WQ on 2016/2/23.
  */
+
+(function () {
+    var querySelector = document.querySelector.bind( document );
+    var querySelectorAll = document.querySelectorAll.bind( document );
+    var bodyHeight;
+    var sections = querySelectorAll( ".section" );
+
+    function loopArray( arr , func ) {
+        for ( var i = 0; i < arr.length; i++ ) {
+            func( arr[ i ] , i );
+        }
+    }
+
+    function resize() {
+        bodyHeight = document.body.offsetHeight;
+        loopArray( sections , function ( setction ) {
+            setction.style.height = bodyHeight + "px";
+        } );
+    }
+
+    function init() {
+        resize();
+    }
+
+    init();
+})();
