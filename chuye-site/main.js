e1c722282231dd36bebbe535e0393a2011e4eb78
WQ <niuyiwangqun@sohu.com>
2016-02-25 16:51:49 +0800

    c

--- chuye-site/main.js
+++ chuye-site/main.js
@@ -74,14 +74,79 @@
         }
     }
 
-    bindEvent( document , "mousewheel" , function ( e ) {
-        wheelScroll( e.wheelDelta );
-    } );
-    bindEvent( document , "DOMMouseScroll" , function ( e ) {
-        wheelScroll( -e.detail );
-    } );
+    function animate( duration , frame , onEnd ) {
+        var s = (new Date()).getTime();
+        setTimeout( function () {
+            var cur = (new Date()).getTime();
+            if ( cur - s < duration ) {
+                frame( (cur - s ) / duration );
+                setTimeout( arguments.callee , 20 );
+            } else {
+                frame( 1 );
+                onEnd();
+            }
+        } , 20 );
+    }
+
+    function Timer( duration , func ) {
+        var timeID;
+        timeID = setTimeout( function () {
+            func();
+            timeID = setTimeout( arguments.callee , duration );
+        } , duration );
+        return {
+            remove : function () {
+                clearTimeout( timeID );
+            }
+        }
+    }
+
+    function initPage0() {
+        var curIndex = 0;
+        var slideWrapper = querySelector( ".page0-img-border-wrapper" );
+        var slideBorder = querySelector( ".page0-img-border" );
+        var circles = querySelectorAll( ".page0-circle" );
+
+        loopArray( querySelectorAll( ".page0-img-border-wrapper .item" ) , function ( item , i ) {
+            item.style.left = 25 * i + "%";
+        } );
+        function slide() {
+            circles[ curIndex ].classList.remove( "select" );
+            circles[ curIndex + 1 == 3 ? 0 : curIndex + 1 ].classList.add( "select" );
+            animate( 400 , function ( percent ) {
+                slideWrapper.style.transform = "translate3d(-" + (25 * curIndex + 25 * percent * percent) + "%,0,0)"
+            } , function () {
+                curIndex += 1;
+                if ( curIndex == 3 ) {
+                    // 从头开始
+                    curIndex = 0;
+                    slideWrapper.style.transform = "translate3d(0,0,0)"
+                }
+            } );
+        }
+
+        //定时轮播
+        var timerHandler = Timer( 4000 , slide );
+
+        slideBorder.onmouseover = sections[ 0 ].stop = function () {
+            // 当页面移出时，要停止轮播图
+            timerHandler.remove();
+        };
+
+        slideBorder.onmouseout = sections[ 0 ].play = function () {
+            // 当页面重新移入时，要继续轮播图
+            timerHandler = Timer( 4000 , slide );
+        };
+
+    }
 
     function init() {
+        bindEvent( document , "mousewheel" , function ( e ) {
+            wheelScroll( e.wheelDelta );
+        } );
+        bindEvent( document , "DOMMouseScroll" , function ( e ) {
+            wheelScroll( -e.detail );
+        } );
         // 第一页的小图标，鼠标放上时候要变化
         loopArray( querySelectorAll( ".page1-icon" ) , function ( icon ) {
             var src = icon.src;
@@ -103,13 +168,8 @@
             }
         } );
         //page-0
-        loopArray( querySelectorAll( ".page0-img-border-wrapper .item" ) , function ( item , i ) {
-            item.style.left = 20 * (i - 1) + "%";
-        } );
+        initPage0();
         resize();
-        document.addEventListener( "mousewheel" , function ( e ) {
-            console.log( "data:" + e.detail );//wheelDelta
-        } );
     }
 
     init();
