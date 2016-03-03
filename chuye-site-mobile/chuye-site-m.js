68fe3098d573a5c8d857ec35fe45d2e105e62352
WQ <niuyiwangqun@sohu.com>
2016-03-03 14:07:58 +0800

    page0

--- chuye-site-mobile/chuye-site-m.js
+++ chuye-site-mobile/chuye-site-m.js
@@ -1,3 +1,35 @@
 /**
  * Created by WQ on 2016/3/3.
  */
+
+(function () {
+    var Height = document.body.offsetHeight;
+    var querySelectorAll = document.querySelectorAll.bind( document );
+    var querySelector = document.querySelector.bind( document );
+    var pages = document.querySelectorAll( ".page" );
+
+    function loopArray( arr , func ) {
+        for ( var i = 0; i < arr.length; i++ ) {
+            func( arr[ i ] , i );
+        }
+    }
+
+    function loopObj( obj , func ) {
+        for ( var key in obj ) {
+            func( obj[ key ] , key );
+        }
+    }
+
+    function css( el , styles ) {
+        loopObj( styles , function ( value , key ) {
+            el.style.setProperty( key , value );
+        } );
+    }
+
+    loopArray( pages , function ( p ) {
+        css( p , {
+            height : Height + "px"
+        } )
+    } );
+
+})();
