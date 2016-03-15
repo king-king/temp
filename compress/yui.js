4d57fb85646cb9554aa1d3ffc05fe107ed796828
WQ <niuyiwangqun@sohu.com>
2016-03-15 14:36:32 +0800

    c

---
+++ compress/yui.js
@@ -0,0 +1,20 @@
+/**
+ * Created by WQ on 2016/3/1.
+ */
+var compressor = require( 'yuicompressor' );
+var fs = require( "fs" );
+compressor.compress( "chuye-site.js" , {
+    //Compressor Options:
+    charset : 'utf8' ,
+    type : 'js' ,
+    'line-break' : 80
+} , function ( err , data , extra ) {
+    if ( !err ) {
+        fs.writeFile( "chuye-site.min.js" , data , function () {
+            console.log( "over" )
+        } )
+    }
+    //err   If compressor encounters an error, it's stderr will be here
+    //data  The compressed string, you write it out where you want it
+    //extra The stderr (warnings are printed here in case you want to echo them
+} );
