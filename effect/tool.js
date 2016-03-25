07ca43890b5a9b5454bb6acc605322d63ccab51a
WQ <niuyiwangqun@sohu.com>
2016-03-25 16:29:14 +0800

    c

--- effect/tool.js
+++ effect/tool.js
@@ -25,3 +25,49 @@ function css( el , style ) {
         el.style.setProperty( key , value );
     } );
 }
+
+function insertCssAnimation() {
+    var style = document.createElement( "style" );
+    document.head.appendChild( style );
+    return function ( arg ) {
+        var name = arg[ 2 ] + " keyframe" + style.sheet.rules.length;
+        var s = "@keyframes keyframe" + style.sheet.rules.length + "{";
+        loopObj( arg[ 1 ] , function ( percent , value ) {
+            s += (percent + "%" + JSON.stringify( value ) );
+        } );
+        s += "}";
+        for ( var i = 3; i < arg.length; i++ ) {
+            name += " " + arg[ i ] + " ";
+        }
+        arg[ 0 ].style.setProperty( "animation" , name );
+        while ( s.indexOf( "\"" ) != -1 ) {
+            s = s.replace( "\"" , "" );
+        }
+        while ( s.indexOf( ",transform" ) != -1 ) {
+            s = s.replace( ",transform" , ";transform" );
+        }
+        style.sheet.insertRule( s , style.sheet.rules.length );
+    }
+}
+
+var cssAnimation = insertCssAnimation();
+
+function $( tag , arg , parent ) {
+    var el = document.createElement( tag );
+    loopObj( arg , function ( key , value ) {
+        switch ( key ) {
+            case "css":
+                css( el , value );
+                break;
+            case "classList":
+                loopArray( value , function ( v ) {
+                    el.classList.add( v );
+                } );
+                break;
+            default:
+                el[ key ] = value;
+        }
+    } );
+    parent && parent.appendChild( el );
+    return el;
+}
