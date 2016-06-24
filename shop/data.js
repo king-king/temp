faa40e8b1e92df50b1df03cb4d69f4f683c58541
king-king <niuyiwangqun@sohu.com>
2016-06-24 14:55:56 +0800

    c

--- shop/data.js
+++ shop/data.js
@@ -4,170 +4,170 @@
 var data = [
     {
         id: 1,
-        cover: "img/IMG_3078.JPG",
+        cover: "image/s1/IMG_3078.JPG",
         text: "有朋友问到了,于是发一波吧,KENZO平底鞋,身边很多妹子在穿,七折!!这么给力的折扣不常有,想要的抓紧按图来问尺码。",
         imgs: [
-            "img/IMG_3078.JPG",
-            "img/IMG_3079.JPG",
-            "img/IMG_3080.JPG",
-            "img/IMG_3081.JPG",
-            "img/IMG_3082.JPG",
-            "img/IMG_3083.JPG",
-            "img/IMG_3084.JPG",
-            "img/IMG_3085.JPG",
-            "img/IMG_3086.JPG"
+            "image/s1/IMG_3078.JPG",
+            "image/s1/IMG_3079.JPG",
+            "image/s1/IMG_3080.JPG",
+            "image/s1/IMG_3081.JPG",
+            "image/s1/IMG_3082.JPG",
+            "image/s1/IMG_3083.JPG",
+            "image/s1/IMG_3084.JPG",
+            "image/s1/IMG_3085.JPG",
+            "image/s1/IMG_3086.JPG"
         ]
     },
     {
         id: 2,
-        cover: "img/IMG_3087.JPG",
+        cover: "image/s2/IMG_3087.JPG",
         text: "adidas nmd纯白,原价1899,现价只要1599美国直邮。所有号码都有哦!数量有限!这个价格还犹豫什么?",
         imgs: [
-            "img/IMG_3087.JPG",
-            "img/IMG_3088.JPG",
-            "img/IMG_3089.JPG",
-            "img/IMG_3090.JPG"
+            "image/s2/IMG_3087.JPG",
+            "image/s2/IMG_3088.JPG",
+            "image/s2/IMG_3089.JPG",
+            "image/s2/IMG_3090.JPG"
         ]
     },
     {
         id: 3,
-        cover: "img/IMG_3091.JPG",
+        cover: "image/s3/IMG_3091.JPG",
         text: "阿迪AlphaBOUNCE,现货男款41、42。女款爆火,配色36两双,现价1199。其它号码微信联系呦。",
         imgs: [
-            "img/IMG_3091.JPG",
-            "img/IMG_3092.JPG",
-            "img/IMG_3093.JPG",
-            "img/IMG_3094.JPG",
-            "img/IMG_3095.JPG"
+            "image/s3/IMG_3091.JPG",
+            "image/s3/IMG_3092.JPG",
+            "image/s3/IMG_3093.JPG",
+            "image/s3/IMG_3094.JPG",
+            "image/s3/IMG_3095.JPG"
         ]
     },
     {
         id: 4,
-        cover: "img/IMG_3096.JPG",
+        cover: "image/s4/IMG_3096.JPG",
         text: "号外!!Longchamp龙骧包,又称\"饺子包\"。由高级帆布或尼龙制成,重量轻容量大,并且耐磨防水,上班族人手一个。多买更有叠加优惠,请私信米果询价。",
         imgs: [
-            "img/IMG_3096.JPG",
-            "img/IMG_3097.JPG",
-            "img/IMG_3098.JPG",
-            "img/IMG_3099.JPG",
-            "img/IMG_3100.JPG",
-            "img/IMG_3101.JPG",
-            "img/IMG_3102.JPG",
-            "img/IMG_3103.JPG",
-            "img/IMG_3104.JPG"
+            "image/s4/IMG_3096.JPG",
+            "image/s4/IMG_3097.JPG",
+            "image/s4/IMG_3098.JPG",
+            "image/s4/IMG_3099.JPG",
+            "image/s4/IMG_3100.JPG",
+            "image/s4/IMG_3101.JPG",
+            "image/s4/IMG_3102.JPG",
+            "image/s4/IMG_3103.JPG",
+            "image/s4/IMG_3104.JPG"
         ]
     },
     {
         id: 5,
-        cover: "img/IMG_3105.JPG",
+        cover: "image/s5/IMG_3105.JPG",
         text: "不得不承认今年最火的时尚单品是ta,丑!拖!鞋!明星都在穿,不用问为什么,来一双就对了。",
         imgs: [
-            "img/IMG_3105.JPG",
-            "img/IMG_3106.JPG",
-            "img/IMG_3107.JPG",
-            "img/IMG_3108.JPG",
-            "img/IMG_3109.JPG",
-            "img/IMG_3110.JPG",
-            "img/IMG_3111.JPG",
-            "img/IMG_3112.JPG"
+            "image/s5/IMG_3105.JPG",
+            "image/s5/IMG_3106.JPG",
+            "image/s5/IMG_3107.JPG",
+            "image/s5/IMG_3108.JPG",
+            "image/s5/IMG_3109.JPG",
+            "image/s5/IMG_3110.JPG",
+            "image/s5/IMG_3111.JPG",
+            "image/s5/IMG_3112.JPG"
         ]
     },
     {
         id: 6,
-        cover: "img/IMG_3113.JPG",
+        cover: "image/s6/IMG_3113.JPG",
         text: "乔12黑红病倒,42号今天发出,乔丹在97年总决赛在第五场穿着乔12黑红拿下38分,并带领公牛战胜爵士。当时MJ称身体不适像是得了感冒,此鞋遂命名为flu-game。今年复刻" +
         "后热度持续上升。感兴趣的朋友们随时提供您需要的鞋码,我们会在第一时间为您寄出。",
         imgs: [
-            "img/IMG_3113.JPG",
-            "img/IMG_3114.JPG",
-            "img/IMG_3115.JPG",
-            "img/IMG_3116.JPG",
-            "img/IMG_3117.JPG",
-            "img/IMG_3118.JPG"
+            "image/s6/IMG_3113.JPG",
+            "image/s6/IMG_3114.JPG",
+            "image/s6/IMG_3115.JPG",
+            "image/s6/IMG_3116.JPG",
+            "image/s6/IMG_3117.JPG",
+            "image/s6/IMG_3118.JPG"
         ]
     },
     {
         id: 7,
-        cover: "img/IMG_3119.JPG",
+        cover: "image/s7/IMG_3119.JPG",
         text: "乔6 胭脂红,有意者请私信询价",
         imgs: [
-            "img/IMG_3119.JPG",
-            "img/IMG_3120.JPG",
-            "img/IMG_3121.JPG",
-            "img/IMG_3122.JPG",
-            "img/IMG_3123.JPG"
+            "image/s7/IMG_3119.JPG",
+            "image/s7/IMG_3120.JPG",
+            "image/s7/IMG_3121.JPG",
+            "image/s7/IMG_3122.JPG",
+            "image/s7/IMG_3123.JPG"
         ]
     },
     {
         id: 8,
-        cover: "img/IMG_3124.JPG",
+        cover: "image/s8/IMG_3124.JPG",
         text: "NB574 夏季新款上市",
         imgs: [
-            "img/IMG_3124.JPG",
-            "img/IMG_3125.JPG",
-            "img/IMG_3126.JPG",
-            "img/IMG_3127.JPG",
-            "img/IMG_3128.JPG"
+            "image/s8/IMG_3124.JPG",
+            "image/s8/IMG_3125.JPG",
+            "image/s8/IMG_3126.JPG",
+            "image/s8/IMG_3127.JPG",
+            "image/s8/IMG_3128.JPG"
         ]
     },
     {
         id: 9,
-        cover: "img/IMG_3129.JPG",
+        cover: "image/s9/IMG_3129.JPG",
         text: "Air Jordan 6 Sports Blue[乔6 蓝白]2014年发售,三年连续火爆,性价比超高,人气爆棚,42.5号现货,1999",
         imgs: [
-            "img/IMG_3129.JPG",
-            "img/IMG_3130.JPG",
-            "img/IMG_3131.JPG",
-            "img/IMG_3132.JPG"
+            "image/s9/IMG_3129.JPG",
+            "image/s9/IMG_3130.JPG",
+            "image/s9/IMG_3131.JPG",
+            "image/s9/IMG_3132.JPG"
         ]
     },
     {
         id: 10,
-        cover: "img/IMG_3133.JPG",
+        cover: "image/s10/IMG_3133.JPG",
         text: "Adidas Superstar[阿迪达斯金标]三色如图,有意购买的小伙伴请转发本条至朋友圈,凭截图可立减20哦",
         imgs: [
-            "img/IMG_3133.JPG",
-            "img/IMG_3134.JPG",
-            "img/IMG_3135.JPG",
-            "img/IMG_3136.JPG",
-            "img/IMG_3137.JPG",
-            "img/IMG_3138.JPG",
-            "img/IMG_3139.JPG",
-            "img/IMG_3140.JPG"
+            "image/s10/IMG_3133.JPG",
+            "image/s10/IMG_3134.JPG",
+            "image/s10/IMG_3135.JPG",
+            "image/s10/IMG_3136.JPG",
+            "image/s10/IMG_3137.JPG",
+            "image/s10/IMG_3138.JPG",
+            "image/s10/IMG_3139.JPG",
+            "image/s10/IMG_3140.JPG"
         ]
     },
     {
         id: 11,
-        cover: "img/IMG_3143.JPG",
+        cover: "image/s11/IMG_3143.JPG",
         text: "Adidas Stan Smith 街拍必备,明星都在穿[阿迪达斯 绿尾]739 尺码不限",
         imgs: [
-            "img/IMG_3143.JPG",
-            "img/IMG_3144.JPG",
-            "img/IMG_3145.JPG",
-            "img/IMG_3146.JPG"
+            "image/s11/IMG_3143.JPG",
+            "image/s11/IMG_3144.JPG",
+            "image/s11/IMG_3145.JPG",
+            "image/s11/IMG_3146.JPG"
         ]
     },
     {
         id: 12,
-        cover: "img/IMG_3147.JPG",
+        cover: "image/s12/IMG_3147.JPG",
         text: "Adidas NMD R1 蓝 六月最新款!!46 现货 1399,其他颜色及号码欢迎询价",
         imgs: [
-            "img/IMG_3147.JPG",
-            "img/IMG_3148.JPG",
-            "img/IMG_3149.JPG",
-            "img/IMG_3150.JPG"
+            "image/s12/IMG_3147.JPG",
+            "image/s12/IMG_3148.JPG",
+            "image/s12/IMG_3149.JPG",
+            "image/s12/IMG_3150.JPG"
         ]
     },
     {
         id: 13,
-        cover: "img/IMG_3151.JPG",
+        cover: "image/s13/IMG_3151.JPG",
         text: "Air Jordan 11 Low 蓝色Navy[乔11 低帮 海蓝]六月发售 男款41号 现货 1899",
         imgs: [
-            "img/IMG_3151.JPG",
-            "img/IMG_3152.JPG",
-            "img/IMG_3153.JPG",
-            "img/IMG_3154.JPG",
-            "img/IMG_3155.JPG"
+            "image/s13/IMG_3151.JPG",
+            "image/s13/IMG_3152.JPG",
+            "image/s13/IMG_3153.JPG",
+            "image/s13/IMG_3154.JPG",
+            "image/s13/IMG_3155.JPG"
         ]
     }
 ];
