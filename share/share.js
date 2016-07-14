7e781752de4599def53170dc7d0a881662b9fe47
king-king <niuyiwangqun@sohu.com>
2016-07-14 15:40:56 +0800

    研究分享代码

--- share/share.js
+++ share/share.js
@@ -3,7 +3,7 @@
  */
 window._bd_share_main ? window._bd_share_is_recently_loaded = !0 : (window._bd_share_is_recently_loaded = !1, window._bd_share_main = {
     version: "2.0",
-    jscfg: {domain: {staticUrl: ""}}
+    jscfg: {domain: {staticUrl: "http://bdimg.share.baidu.com/"}}
 }), !window._bd_share_is_recently_loaded && (window._bd_share_main.F = window._bd_share_main.F || function (e, t) {
         function r(e, t) {
             if (e instanceof Array) {
