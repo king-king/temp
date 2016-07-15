563656753e0d3205329fd6cf326dc4e847b190e8
king-king <niuyiwangqun@sohu.com>
2016-07-15 11:16:36 +0800

    研究分享代码

--- share/plugin.client.js
+++ share/plugin.client.js
@@ -1,18 +1,111 @@
-/* (c) 2016 JiaThis Inc. 0331*/
-eval(function (p, a, c, k, e, r) {
-    e = function (c) {
-        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
-    };
-    if (!''.replace(/^/, String)) {
-        while (c--)r[e(c)] = k[c] || e(c);
-        k = [function (e) {
-            return r[e]
-        }];
-        e = function () {
-            return '\\w+'
-        };
-        c = 1
+(function () {
+    var d = document, w = window, dd = d.documentElement, db = d.body, head = d.getElementsByTagName("head")[0] || dd, ws = w.screen, dm = w.location.host || "", dm = dm.replace(/^www\./, ''), na = navigator, ua = na.userAgent.toLowerCase(), se = {
+        baidu: 'wd',
+        google: 'q',
+        haosou: 'q',
+        soso: 'w',
+        sogou: 'query',
+        yahoo: 'p',
+        youdao: 'q',
+        iask: 'k',
+        'sina': 'key',
+        zhongsou: 'w',
+        bing: 'q',
+        so: 'q'
+    }, dms = {
+        cn: 'com|org|gov|net',
+        tw: 'com',
+        hk: 'com'
+    }, ec = encodeURIComponent, H = 'http://lc.jiathis.com', MU = H + '/c.gif', idurl = 'http://id.jiathis.com/id.php', p,
+    intsallUyan = 0, intsallUjian = 0, scr = d.getElementsByTagName('script');
+    for (var i = 0, ss; ss = scr[i++];) {
+        if (ss.src) {
+            if (ss.src.indexOf('jia.js') != -1 || ss.src.indexOf('jiathis_r.js') != -1) {
+                intsallJiathis = 1
+            }
+            if (ss.src.indexOf('uyan.js') != -1 || ss.src.indexOf('uyan.cc/js/iframe.js') != -1) {
+                intsallUyan = 1
+            }
+            if (ss.src.indexOf('ujian.js') != -1) {
+                intsallUjian = 1
+            }
+            if (ss.src.indexOf('plugin.client.js') != -1) {
+                ss.src.replace(/(u|t|r|f)=([^&]+)/g, function (a, p, v) {
+                    params[p] = v
+                })
+            }
+        }
     }
-    while (c--)if (k[c])p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-    return p
-}('(4(){3 d=28,w=1K,1c=d.1A,10=d.1v,V=d.16("V")[0]||1c,2e=w.29,J=w.17.D||"",J=J.x(/^11\\./,\'\'),1k=1L,1R=1k.1s.1r(),X={1U:\'24\',27:\'q\',1u:\'q\',1w:\'w\',1y:\'1z\',1G:\'p\',1M:\'q\',1Q:\'k\',\'1T\':\'N\',1V:\'w\',1Y:\'q\',20:\'q\'},P={25:\'F|2c|2g|2j\',1n:\'F\',1q:\'F\'},G=1t,H=\'1i://1x.Q.F\',1h=H+\'/c.1J\',1f=\'1i://T.Q.F/T.1P\',7={},W=0,Y=0,Z=0,1b=d.16(\'1a\');18(3 i=0,9;9=1b[i++];){5(9.6){5(9.6.B(\'2d.y\')!=-1||9.6.B(\'2i.y\')!=-1){W=1}5(9.6.B(\'U.y\')!=-1||9.6.B(\'U.2p/y/2r.y\')!=-1){Y=1}5(9.6.B(\'15.y\')!=-1){Z=1}5(9.6.B(\'1o.1p.y\')!=-1){9.6.x(/(u|t|r|f)=([^&]+)/g,4(a,p,v){7[p]=v})}}}w.13={12:4(o,t,a){3 b=d.1B(t||"1C");18(3 p 1D o){p=="1E"?(b[p].1F=o[p]):(b[p]=o[p])}m(a||10).1H(b,(a||10).1I)},O:4(s){m s.x(/^\\s+|\\s+$/g,\'\')},14:4(s,n,d){s=n==\'p\'?s.x("1N=","1O="):s;m s.C(E I(n.x(/([\\[\\]])/g,\'\\\\\\$1\')+"=([^&\\\\?#]*)"),\'1S\')?I.$1:d||K},19:4(h,g){3 k=h.1W(\'.\').1X(),i=0,l=k.1Z,o,L;21{o=k[i];L=k[i+1];5(P[o]&&L&&L.C(P[o])){22}}23(++i<l);o=k[i<l?i+2:1];5(g&&o){o="."+h.C(E I(".*("+o+".*?)$"))[1]}m o},1d:4(r){5(r.C(/(?:[a-z]\\:\\/\\/)([^\\/\\?]+)(.*)/26)){3 o={D:I.$1,1e:I.$2},h=e.19(o.D),k;5(X[h]){k=e.14(o.1e,X[h]);o.N=k;o.A=h}m o}m 0},2a:4(o){m o&&2b o!=\'1g\'&&o!=\'1g\'},R:4(){3 r=e.O(7[\'f\']==\'j\'?(7[\'r\']!=K?7[\'r\']:\'\'):d.2f);3 a=e.O(1j(7[\'f\']==\'j\'?(7[\'t\']!=K?7[\'t\']:\'\'):d.2h));3 b=e.O(1j(7[\'f\']==\'j\'?(7[\'u\']!=K?7[\'u\']:\'\'):d.17));3 o=e.1d(r),M="",A="",S="",b=\'u=\'+G(b)+\'&t=\'+G(a);5(o&&o.D!=J&&!o.D.C(J.x(/^11/,\'\').x(/\\./g,\'\\\\.\')+\'$\')&&o.A){5(o.N){M=G(o.N)}2k{M=""}A=o.A}5(r){S=G(r)}b+=\'&r=\'+S+\'&k=\'+M+\'&s=\'+A;b+=\'&Q=\'+W+\'&U=\'+Y+\'&15=\'+Z;b+=\'&2l=\'+E 2m().2n();m b},2o:4(a){3 b=e.R();3 c=\'T=\'+a+\'&\'+b;(E 1l).6=1h+"?"+c},2q:4(a){(E 1l).6=a},1m:4(){3 a=e.R();3 b=1f+"?"+a;e.12({6:b,2s:"2t-8"},"1a",V)}};13.1m()})();', 62, 154, '|||var|function|if|src|params||ss|||||this||||||||return|||||||||||replace|js||source|indexOf|match|host|new|com|ec||RegExp|dm|null|ao|keyword|key|trim|dms|jiathis|getReqUrl|ref|id|uyan|head|intsallJiathis|se|intsallUyan|intsallUjian|db|www|creElm|_gnayTrack|getP|ujian|getElementsByTagName|location|for|getDm|script|scr|dd|getRf|path|idurl|undefined|MU|http|String|na|Image|init|tw|plugin|client|hk|toLowerCase|userAgent|encodeURIComponent|haosou|body|soso|lc|sogou|query|documentElement|createElement|div|in|style|cssText|yahoo|insertBefore|firstChild|gif|window|navigator|youdao|fp_ip|fp_ips|php|iask|ua|gm|sina|baidu|zhongsou|split|reverse|bing|length|so|do|break|while|wd|cn|gi|google|document|screen|def|typeof|org|jia|ws|referrer|gov|title|jiathis_r|net|else|time|Date|getTime|req|cc|reqImg|iframe|charset|utf'.split('|'), 0, {}))
+    w._gnayTrack = {
+        creElm: function (o, t, a) {
+            var b = d.createElement(t || "div");
+            for (var p in o) {
+                p == "style" ? (b[p].cssText = o[p]) : (b[p] = o[p])
+            }
+            return (a || db).insertBefore(b, (a || db).firstChild)
+        }, trim: function (s) {
+            return s.replace(/^\s+|\s+$/g, '')
+        }, getP: function (s, n, d) {
+            s = n == 'p' ? s.replace("fp_ip=", "fp_ips=") : s;
+            return s.match(new RegExp(n.replace(/([\[\]])/g, '\\\$1') + "=([^&\\?#]*)"), 'gm') ? RegExp.$1 : d || null
+        }, getDm: function (h, g) {
+            var k = h.split('.').reverse(), i = 0, l = k.length, o, ao;
+            do {
+                o = k[i];
+                ao = k[i + 1];
+                if (dms[o] && ao && ao.match(dms[o])) {
+                    break
+                }
+            } while (++i < l);
+            o = k[i < l ? i + 2 : 1];
+            if (g && o) {
+                o = "." + h.match(new RegExp(".*(" + o + ".*?)$"))[1]
+            }
+            return o
+        }, getRf: function (r) {
+            if (r.match(/(?:[a-z]\:\/\/)([^\/\?]+)(.*)/gi)) {
+                var o = {host: RegExp.$1, path: RegExp.$2}, h = this.getDm(o.host), k;
+                if (se[h]) {
+                    k = this.getP(o.path, se[h]);
+                    o.key = k;
+                    o.source = h
+                }
+                return o
+            }
+            return 0
+        }, def: function (o) {
+            return o && typeof o != 'undefined' && o != 'undefined'
+        }, getReqUrl: function () {
+            var r = this.trim(params['f'] == 'j' ? (params['r'] != null ? params['r'] : '') : d.referrer);
+            var a = this.trim(String(params['f'] == 'j' ? (params['t'] != null ? params['t'] : '') : d.title));
+            var b = this.trim(String(params['f'] == 'j' ? (params['u'] != null ? params['u'] : '') : d.location));
+            var o = this.getRf(r), keyword = "", source = "", ref = "", b = 'u=' + ec(b) + '&t=' + ec(a);
+            if (o && o.host != dm && !o.host.match(dm.replace(/^www/, '').replace(/\./g, '\\.') + '$') && o.source) {
+                if (o.key) {
+                    keyword = ec(o.key)
+                } else {
+                    keyword = ""
+                }
+                source = o.source
+            }
+            if (r) {
+                ref = ec(r)
+            }
+            b += '&r=' + ref + '&k=' + keyword + '&s=' + source;
+            b += '&jiathis=' + intsallJiathis + '&uyan=' + intsallUyan + '&ujian=' + intsallUjian;
+            b += '&time=' + new Date().getTime();
+            return b
+        }, req: function (a) {
+            var b = this.getReqUrl();
+            var c = 'id=' + a + '&' + b;
+            (new Image).src = MU + "?" + c
+        }, reqImg: function (a) {
+            (new Image).src = a
+        }, init: function () {
+            var a = this.getReqUrl();
+            var b = idurl + "?" + a;
+            this.creElm({src: b, charset: "utf-8"}, "script", head)
+        }
+    };
+    _gnayTrack.init()
+})();
