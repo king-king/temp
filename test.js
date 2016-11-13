3922085fdabdf128dae44bf6738d70c6e6f7a292
qunwang <niuyiwangqun@sohu.com>
2016-11-13 22:18:07 +0800

    写出一个新的匹配

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 var str = "333444555666777888";
-console.log(str.replace(/(\d)(?=(\d{3})+$)/g, "$1,"));
+//console.log(str.replace(/(\d)(?=(\d{3})+$)/g, "$1,"));
+
+
+var str2 = "wangqun.min.js";
+
+console.log(str2.match(/^[a-z]+(?!min)\.js/));
