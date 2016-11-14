59c04b031687a345dad418152b82bf550dd4bdd8
king-king <niuyiwangqun@sohu.com>
2016-11-14 11:25:53 +0800

    完善正则表达式

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var str = "333444555666777888";
 //console.log(str.replace(/(\d)(?=(\d{3})+$)/g, "$1,"));
 
 
-var str2 = "wangqun.min.js";
-
-console.log(str2.match(/^[a-z]+(?!min)\.js/));
+var str2 = "wangqun.min.ok.js";
+// 匹配.js，但不匹配min.js
+console.log(str2.match(/^[a-z.]+(?!min)\.js/));
